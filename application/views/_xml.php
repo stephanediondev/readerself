65cc2e41ad0a7dfaa421afd6c28e8747b88c7022
Stéphane Dion <divers@sdion.net>
2013-03-24 17:59:12 +0100

    -

--- application/views/_xml.php
+++
@@ -1,5 +0,0 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<raesingu>
-<?php if(isset($content) == 1) { echo $content; } ?>
-<?php echo $this->raesingu_library->get_debug(); ?>
-</raesingu>
