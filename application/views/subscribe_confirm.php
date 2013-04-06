9d6bd9db80556298a39ca972d16c8752249f1d26
Stéphane Dion <divers@sdion.net>
2013-04-07 01:38:57 +0200

    -

--- application/views/subscribe_confirm.php
+++ application/views/subscribe_confirm.php
@@ -3,7 +3,7 @@ modal_hide();
 $('.sidebar-nav li').removeClass('active');
 
 $('#sidebar .menu').find('.result').remove();
-content = '<li class="result active"><a id="load-sub-<?php echo $sub_id; ?>-items" href="<?php echo base_url(); ?>home/items/sub/<?php echo $sub_id; ?>"><?php echo addslashes($fed_title); ?> (<span>0</span>)</a></li>';
+content = '<li class="result active"><a id="load-sub-<?php echo $sub_id; ?>-items" href="<?php echo base_url(); ?>home/items/sub/<?php echo $sub_id; ?>"><i class="icon icon-rss"></i><?php echo addslashes($fed_title); ?> (<span>0</span>)</a></li>';
 $('#sidebar .menu').append(content);
 
 load_items('<?php echo base_url(); ?>home/items/sub/<?php echo $sub_id; ?>');
