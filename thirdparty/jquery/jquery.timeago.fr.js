9b487312df67cea860a81c941b9efea2b71a34c4
Stéphane Dion <divers@sdion.net>
2013-08-31 09:49:23 +0200

    -

---
+++ thirdparty/jquery/jquery.timeago.fr.js
@@ -0,0 +1,20 @@
+//fr
+jQuery.timeago.settings.strings = {
+	prefixAgo: "il y a",
+	prefixFromNow: "d'ici",
+	suffixAgo: null,
+	suffixFromNow: null,
+	seconds: "moins d'une minute",
+	minute: "une minute",
+	minutes: "%d minutes",
+	hour: "une heure",
+	hours: "%d heures",
+	day: "un jour",
+	days: "%d jours",
+	month: "un mois",
+	months: "%d mois",
+	year: "un an",
+	years: "%d ans",
+	wordSeparator: " ",
+	numbers: []
+};
