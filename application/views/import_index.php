4f74aeb01d9589f76be5b9f3bc9094cb7f30b4b7
Stéphane Dion <divers@sdion.net>
2013-09-06 21:15:25 +0200

    -

--- application/views/import_index.php
+++ application/views/import_index.php
@@ -1,11 +1,21 @@
+<div id="actions-main">
+	<ul class="actions">
+		<li><a href="<?php echo base_url(); ?>subscriptions"><i class="icon icon-step-backward"></i><?php echo $this->lang->line('back'); ?></a></li>
+	</ul>
+</div>
 <main>
 	<section>
 		<section>
-	<h1><i class="icon icon-download-alt"></i><?php echo $this->lang->line('import'); ?></h1>
-
 	<?php echo validation_errors(); ?>
 
 	<?php echo form_open_multipart(current_url()); ?>
+
+		<article class="cell">
+			<h2><i class="icon icon-rss"></i><?php echo $this->lang->line('subscriptions'); ?></h2>
+		</article>
+
+	<h2><i class="icon icon-download-alt"></i><?php echo $this->lang->line('import'); ?></h2>
+
 	<input type="hidden" name="hidden">
 
 	<p>
