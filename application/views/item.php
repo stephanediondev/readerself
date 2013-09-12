76271baf06482f86b29f5c2b4778974b0b72f71f
Stéphane Dion <divers@sdion.net>
2013-09-12 21:24:20 +0200

    -

--- application/views/item.php
+++ application/views/item.php
@@ -35,29 +35,29 @@
 				<?php foreach($itm->enclosures as $enr) { ?>
 					<?php $filename = substr($enr->enr_link, strrpos($enr->enr_link, '/') + 1); ?>
 					<?php if(stristr($filename, '?')) { ?><?php $filename = substr($filename, 0, strpos($filename, '?'));?><?php } ?>
-					<?php if(stristr($enr->enr_type, 'image/')) { ?>
+					<?php if(stristr($enr->enr_type, 'image/') && $filename != '') { ?>
 						<p><a target="_blank" href="<?php echo $enr->enr_link; ?>"><i class="icon icon-picture"></i><?php echo $filename; ?></a></p>
 						<?php if($enr->enr_length == 0 || $enr->enr_length <= 1048576) { ?><a target="_blank" href="<?php echo $enr->enr_link; ?>"><img src="<?php echo $enr->enr_link; ?>" alt=""></a><?php } ?>
 
-					<?php } else if(stristr($enr->enr_type, 'audio/')) { ?>
+					<?php } else if(stristr($enr->enr_type, 'audio/') && $filename != '') { ?>
 						<p><a target="_blank" href="<?php echo $enr->enr_link; ?>"><i class="icon icon-volume-up"></i><?php echo $filename; ?></a></p>
 						<audio controls>
 							<source src="<?php echo $enr->enr_link; ?>" type="<?php echo $enr->enr_type; ?>">
 						</audio>
 
-					<?php } else if($enr->enr_type == 'video/vimeo') { ?>
+					<?php } else if($enr->enr_type == 'video/vimeo' && $filename != '') { ?>
 						<p><a target="_blank" href="http://vimeo.com/<?php echo $filename; ?>"><i class="icon icon-youtube-play"></i><?php echo $filename; ?> (Vimeo)</a></p>
 						<iframe allowfullscreen src="<?php echo $enr->enr_link; ?>" width="<?php echo $enr->enr_width; ?>" height="<?php echo $enr->enr_height; ?>"></iframe>
 
-					<?php } else if($enr->enr_type == 'video/dailymotion') { ?>
+					<?php } else if($enr->enr_type == 'video/dailymotion' && $filename != '') { ?>
 						<p><a target="_blank" href="http://www.dailymotion.com/video/<?php echo $filename; ?>"><i class="icon icon-youtube-play"></i><?php echo $filename; ?> (Dailymotion)</a></p>
 						<iframe allowfullscreen src="<?php echo $enr->enr_link; ?>" width="<?php echo $enr->enr_width; ?>" height="<?php echo $enr->enr_height; ?>"></iframe>
 
-					<?php } else if($enr->enr_type == 'video/youtube') { ?>
+					<?php } else if($enr->enr_type == 'video/youtube' && $filename != '') { ?>
 						<p><a target="_blank" href="http://www.youtube.com/watch?v=<?php echo $filename; ?>"><i class="icon icon-youtube-play"></i><?php echo $filename; ?> (Youtube)</a></p>
 						<iframe allowfullscreen src="<?php echo $enr->enr_link; ?>" width="<?php echo $enr->enr_width; ?>" height="<?php echo $enr->enr_height; ?>"></iframe>
 
-					<?php } else if(stristr($enr->enr_type, 'video/')) { ?>
+					<?php } else if(stristr($enr->enr_type, 'video/') && $filename != '') { ?>
 						<p><a target="_blank" href="<?php echo $enr->enr_link; ?>"><i class="icon icon-youtube-play"></i><?php echo $filename; ?></a></p>
 						<video width="<?php echo $enr->enr_width; ?>" height="<?php echo $enr->enr_height; ?>" controls>
 							<source src="<?php echo $enr->enr_link; ?>" type="<?php echo $enr->enr_type; ?>">
