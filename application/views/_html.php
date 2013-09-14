d29720c7944280bef0a92130d3bddef14da0f40d
Stéphane Dion <divers@sdion.net>
2013-09-14 09:10:54 +0200

    -

--- application/views/_html.php
+++ application/views/_html.php
@@ -61,7 +61,6 @@ var timezone = true;
 <?php } else { ?>
 var timezone = false;
 <?php } ?>
-var title  = '<?php echo addslashes($this->config->item('title')); ?>';
 var uri_string = '<?php echo $this->uri->uri_string(); ?>';
 </script>
 
