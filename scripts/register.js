8adbd760adc0bc2cbea775e97d3582d1c83e3e82
Stéphane Dion <divers@sdion.net>
2013-09-15 08:15:54 +0200

    no paste on cofirmation fields

--- scripts/register.js
+++ scripts/register.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$('#mbr_email').focus();
 	$('#mbr_password').attr('value', '');
 	$('#mbr_password_confirm').attr('value', '');
+
+	$('#mbr_email_confirm, #mbr_password_confirm').bind('paste', function(event) {
+		event.preventDefault();
+	});
 });
