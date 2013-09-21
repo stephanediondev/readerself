189b4159b6b875b493f2c3909fe8c1fd6c39659b
Stéphane Dion <divers@sdion.net>
2013-09-21 05:41:28 +0200

    -

--- application/views/share_email.php
+++ application/views/share_email.php
@@ -14,17 +14,7 @@
 
 <div<?php if($itm->sub->direction) { ?> dir="<?php echo $itm->sub->direction; ?>"<?php } ?> style="background-color:#FFFFFF;border:1px solid #E3E3E3;border-radius:3px 3px 3px 3px;color:#333333;font-family:Helvetica,Arial,sans-serif;font-size:14px;margin-bottom:0px;margin-left:10px;margin-right:10px;margin-top:10px;padding:10px 10px;">
 	<h2 style="font-size:14px;font-weight:bold;margin:5px 0px;"><a style="color:#777777;" href="<?php echo $itm->itm_link; ?>"><?php echo $itm->itm_title; ?></a></h2>
-	<ul style="margin:5px 0px;padding:0px 20px;">
-		<li><?php echo $itm->explode_date; ?></li>
-		<li><?php echo $itm->explode_time; ?></li>
-		<?php if($itm->itm_author) { ?>
-			<li><?php echo $itm->itm_author; ?></li>
-		<?php } ?>
-		<li><?php echo $itm->sub->title; ?></li>
-		<?php if($this->config->item('tags') && $itm->categories) { ?>
-			<li><?php echo implode(', ', $itm->categories); ?></li>
-		<?php } ?>
-	</ul>
+	<p style="margin:5px 0px;"><?php echo $itm->explode_date; ?> / <?php echo $itm->explode_time; ?><?php if($itm->itm_author) { ?> / <?php echo $itm->itm_author; ?><?php } ?> / <?php echo $itm->sub->title; ?><?php if($this->config->item('tags') && $itm->categories) { ?><br><?php echo implode(', ', $itm->categories); ?><?php } ?></p>
 </div>
 
 <div<?php if($itm->sub->direction) { ?> dir="<?php echo $itm->sub->direction; ?>"<?php } ?> style="background-color:#F5F5F5;border:1px solid #E3E3E3;border-radius:3px 3px 3px 3px;color:#333333;font-family:Helvetica,Arial,sans-serif;font-size:14px;margin-bottom:10px;margin-left:10px;margin-right:10px;margin-top:10px;padding:10px 10px;">
