92a5773bc2d310f67762375960eda6ebee602802
Stéphane Dion <divers@sdion.net>
2013-09-27 20:11:12 +0200

    -

--- scripts/_html.js
+++ scripts/_html.js
@@ -264,14 +264,16 @@ $(document).ready(function() {
 	});
 
 	if($('aside').length > 0) {
-		$(document).swipe({
-			swipeLeft:function(event, direction, distance, duration, fingerCount) {
-				if($('#toggle-sidebar').is(':visible')) {
-					toggle_sidebar();
-				}
-			},
-			threshold: 120
-		});
+		if($('aside').css('position') == 'absolute') {
+			$(document).swipe({
+				swipeLeft:function(event, direction, distance, duration, fingerCount) {
+					if($('#toggle-sidebar').is(':visible')) {
+						toggle_sidebar();
+					}
+				},
+				threshold: 120
+			});
+		}
 	}
 
 	$('.fullscreen').bind('click', function(event) {
