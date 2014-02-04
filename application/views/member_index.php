730f1998dcaafc13e731176dffa04878721030f7
Stéphane Dion <divers@sdion.net>
2014-02-04 19:37:13 +0100

    -

--- application/views/member_index.php
+++ application/views/member_index.php
@@ -1,6 +1,6 @@
 	<nav>
 		<ul class="actions">
-			<li class="hide-phone"><a href="#" title="<?php echo $this->lang->line('title_shift_f'); ?>" class="fullscreen"><i class="icon icon-resize-full"></i><?php echo $this->lang->line('fullscreen'); ?></a></li>
+			<li class="hide-phone"><a href="#" title="<?php echo $this->lang->line('title_shift_f'); ?>" class="fullscreen"><i class="icon icon-resize-full"></i><i class="icon icon-resize-small"></i><?php echo $this->lang->line('fullscreen'); ?></a></li>
 			<li><a href="#" title="r" class="items_refresh"><i class="icon icon-refresh"></i><?php echo $this->lang->line('refresh'); ?></a></li>
 			<?php if($this->input->cookie('items_display') == 'collapse') { ?>
 				<li><a href="#" class="items_display"><span class="expand" title="2" style="display:inline-block;"><i class="icon icon-collapse"></i><?php echo $this->lang->line('expand'); ?></span><span class="collapse" title="1" style="display:none;"><i class="icon icon-collapse-top"></i><?php echo $this->lang->line('collapse'); ?></span></a></li>
@@ -12,11 +12,6 @@
 		</ul>
 	</nav>
 </header>
-<div id="fullscreen_back">
-	<ul class="actions">
-		<li class="hide-phone"><a href="#" title="<?php echo $this->lang->line('title_shift_f'); ?>" class="fullscreen"><i class="icon icon-resize-small"></i><?php echo $this->lang->line('fullscreen'); ?></a></li>
-	</ul>
-</div>
 <main>
 	<section>
 		<section>
