b2a35ef31449df122e0e78780d6413a70ef3cea9
project29k <divers@sdion.net>
2015-01-23 04:34:21 +0100

    -

--- application/views/profile_connections.php
+++ application/views/profile_connections.php
@@ -1,9 +1,3 @@
-<?php
-namespace UAS;
-require 'thirdparty/uasparser/Parser.php';
-$parser = new Parser();
-$parser->SetCacheDir('application/cache/');
-?>
 	<nav>
 		<ul class="actions">
 			<li><a href="<?php echo base_url(); ?>profile"><i class="icon icon-step-backward"></i><?php echo $this->lang->line('back'); ?></a></li>
@@ -21,15 +15,14 @@ $parser->SetCacheDir('application/cache/');
 
 	<?php if($connections) { ?>
 		<?php foreach($connections as $cnt) { ?>
-		<?php $ua_info = $parser->parse($cnt->cnt_agent); ?>
 		<?php list($date, $time) = explode(' ', $cnt->cnt_datecreated); ?>
 			<article<?php if($this->member->token_connection == $cnt->token_connection) { ?> class="item-selected"<?php } ?>>
-				<h2><i class="icon icon-signin"></i><?php echo $ua_info['os_name']; ?> / <?php echo $ua_info['ua_name']; ?> <?php if($this->member->token_connection == $cnt->token_connection) { ?> / <em><?php echo $this->lang->line('current_connection'); ?></em><?php } ?></h2>
+				<h2><i class="icon icon-signin"></i><?php echo $cnt->cnt_agent; ?></h2>
 				<ul class="item-details">
 					<li><i class="icon icon-bolt"></i><?php echo $cnt->cnt_ip; ?></li>
 					<li><i class="icon icon-calendar"></i><?php echo $date; ?></li>
 					<li><i class="icon icon-time"></i><?php echo $time; ?> (<span class="timeago" title="<?php echo $this->readerself_library->timezone_datetime($cnt->cnt_datecreated); ?>"></span>)</li>
-					<li class="block"><?php echo $cnt->cnt_agent; ?></li>
+					<?php if($this->member->token_connection == $cnt->token_connection) { ?><li><strong><?php echo $this->lang->line('current_connection'); ?></strong></li><?php } ?>
 				</ul>
 			</article>
 		<?php } ?>
