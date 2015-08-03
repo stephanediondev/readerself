15c579160539156173e97e97f079f3f50c1eb306
project29k <divers@sdion.net>
2015-08-03 12:55:44 +0200

    -

--- application/views/subscriptions_index.php
+++ application/views/subscriptions_index.php
@@ -13,50 +13,50 @@
 <main class="mdl-layout__content mdl-color--grey-100">
 	<div class="mdl-grid">
 		<div class="mdl-card mdl-cell mdl-cell--12-col">
-			<div class="mdl-card__title">
+			<div class="mdl-card__title mdl-color-text--white mdl-color--teal">
 				<h1 class="mdl-card__title-text"><?php echo $this->lang->line('subscriptions'); ?> (<?php echo $position; ?>)</h1>
 			</div>
 			<div class="mdl-card__actions mdl-card--border">
-				<a href="javascript:void(function(){window.open('<?php echo base_url(); ?>?u='+encodeURIComponent(window.location.href),'_blank');}());"><i class="icon icon-bookmark"></i><?php echo $this->config->item('title'); ?> (bookmarklet)</a>
-				<a href="#"><i class="icon icon-save"></i><?php echo $this->lang->line('registerContentHandler'); ?></a>
-				<a href="<?php echo base_url(); ?>subscriptions/create"><i class="icon icon-plus"></i><?php echo $this->lang->line('add'); ?></a>
-				<a href="<?php echo base_url(); ?>subscriptions/import"><i class="icon icon-download-alt"></i><?php echo $this->lang->line('import'); ?></a>
-				<a href="<?php echo base_url(); ?>subscriptions/export"><i class="icon icon-upload-alt"></i><?php echo $this->lang->line('export'); ?></a>
+				<a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" href="<?php echo base_url(); ?>subscriptions/create"><i class="material-icons md-18">add</i></a>
+				<a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" href="<?php echo base_url(); ?>subscriptions/import"><i class="material-icons md-18">file_download</i></a>
+				<a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" href="<?php echo base_url(); ?>subscriptions/export"><i class="material-icons md-18">file_upload</i></a>
 			</div>
 		</div>
 
 <?php if($subscriptions) { ?>
 	<?php foreach($subscriptions as $sub) { ?>
-		<div<?php if($sub->sub_direction) { ?> dir="<?php echo $sub->sub_direction; ?>"<?php } else if($sub->fed_direction) { ?> dir="<?php echo $sub->fed_direction; ?>"<?php } ?> class="mdl-card mdl-cell mdl-cell--6-col">
+		<div<?php if($sub->sub_direction) { ?> dir="<?php echo $sub->sub_direction; ?>"<?php } else if($sub->fed_direction) { ?> dir="<?php echo $sub->fed_direction; ?>"<?php } ?> class="mdl-card mdl-cell mdl-cell--4-col">
 			<div class="mdl-card__title">
 				<h1 class="mdl-card__title-text"><a style="background-image:url(https://www.google.com/s2/favicons?domain=<?php echo $sub->fed_host; ?>&amp;alt=feed);" class="favicon" href="<?php echo base_url(); ?>subscriptions/read/<?php echo $sub->sub_id; ?>"><?php echo $sub->fed_title; ?><?php if($sub->sub_title) { ?> / <em><?php echo $sub->sub_title; ?></em><?php } ?></a></h1>
 				<div class="mdl-card__title-infos">
-					<?php if($this->config->item('folders')) { ?>
-						<?php if($sub->flr_title) { ?><a href="<?php echo base_url(); ?>folders/read/<?php echo $sub->flr_id; ?>"><i class="icon icon-folder-close"></i><?php echo $sub->flr_title; ?></a><?php } else { ?><i class="icon icon-folder-close"></i><em><?php echo $this->lang->line('no_folder'); ?></em><?php } ?>
-					<?php } ?>
 					<?php if($sub->fed_url) { ?>
-						<a href="<?php echo $sub->fed_url; ?>" target="_blank"><i class="icon icon-external-link"></i><?php echo $sub->fed_url; ?></a>
+						<a class="mdl-navigation__link" href="<?php echo $sub->fed_url; ?>" target="_blank"><i class="material-icons md-16">open_in_new</i><?php echo $sub->fed_url; ?></a>
 					<?php } ?>
-					<?php if($this->config->item('tags') && $sub->categories) { ?>
-						<i class="icon icon-tags"></i><?php echo implode(', ', $sub->categories); ?>
+					<?php if($this->config->item('folders')) { ?>
+						<?php if($sub->flr_title) { ?><a class="mdl-navigation__link" href="<?php echo base_url(); ?>folders/read/<?php echo $sub->flr_id; ?>"><i class="material-icons md-16">folder</i><?php echo $sub->flr_title; ?></a><?php } ?>
 					<?php } ?>
 					<?php if($sub->fed_lasterror) { ?>
 						<i class="icon icon-bell"></i><?php echo $sub->fed_lasterror; ?>
 					<?php } ?>
 				</div>
 			</div>
-		<div class="mdl-card__supporting-text mdl-color-text--grey">
-			<?php echo $sub->fed_description; ?>
+			<div class="mdl-card__supporting-text mdl-color-text--grey">
+				<?php if($this->config->item('tags') && $sub->categories) { ?>
+					<p><?php echo implode(', ', $sub->categories); ?></p>
+				<?php } ?>
+				<p><?php echo $sub->fed_description; ?></p>
+			</div>
+			<div class="mdl-card__actions mdl-card--border">
+				<a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon subscribe" href="<?php echo base_url(); ?>subscriptions/subscribe/<?php echo $sub->sub_id; ?>"><i class="material-icons md-18">bookmark</i></a>
+				<a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon priority" href="<?php echo base_url(); ?>subscriptions/priority/<?php echo $sub->sub_id; ?>"><?php if($sub->sub_priority == 0) { ?><i class="material-icons md-18">chat_bubble_outline</i><?php } ?><?php if($sub->sub_priority == 1) { ?><i class="material-icons md-18">announcement</i><?php } ?></a>
+				<a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" href="<?php echo base_url(); ?>subscriptions/update/<?php echo $sub->sub_id; ?>"><i class="material-icons md-18">mode_edit</i></a>
+			</div>
 		</div>
-		<div class="mdl-card__actions mdl-card--border">
-			<a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon subscribe" href="<?php echo base_url(); ?>subscriptions/subscribe/<?php echo $sub->sub_id; ?>"><i class="material-icons md-18">bookmark</i></a>
-			<a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon priority" href="<?php echo base_url(); ?>subscriptions/priority/<?php echo $sub->sub_id; ?>"><?php if($sub->sub_priority == 0) { ?><i class="material-icons md-18">chat_bubble_outline</i><?php } ?><?php if($sub->sub_priority == 1) { ?><i class="material-icons md-18">announcement</i><?php } ?></a>
-			<a class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" href="<?php echo base_url(); ?>subscriptions/update/<?php echo $sub->sub_id; ?>"><i class="material-icons md-18">mode_edit</i></a>
-		</div>
-	</div>
 	<?php } ?>
-	<div class="paging">
-		<?php echo $pagination; ?>
+	<div class="mdl-card mdl-cell mdl-cell--12-col paging">
+		<div class="mdl-card__supporting-text mdl-color-text--grey">
+			<?php echo $pagination; ?>
+		</div>
 	</div>
 <?php } ?>
 	</div>
