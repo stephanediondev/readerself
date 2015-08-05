0a824529774800cb45131d5482fac01e45a47ce2
project29k <divers@sdion.net>
2015-08-05 02:37:24 +0200

    -

--- scripts/member.js
+++ scripts/member.js
@@ -13,7 +13,7 @@ var lock_no_more_items = false;
 function load_items(url) {
 	url = url + '/?items_mode=' + items_mode + '&items_display=' + items_display;
 	$('.mdl-layout__content').scrollTop(0);
-	$('.mdl-grid').html('<div class="mdl-spinner mdl-js-spinner is-active"></div>');
+	$('.mdl-grid').html('<div class="mdl-spinner mdl-js-spinner is-active"></div>');componentHandler.upgradeDom('MaterialSpinner', 'mdl-spinner');
 	var params = [];
 	params.push({'name': csrf_token_name, 'value': $.cookie(csrf_cookie_name)});
 	pagination = 0;
@@ -66,7 +66,7 @@ function add_items(url) {
 	if(!lock_add_items && !lock_no_more_items) {
 		lock_add_items = true;
 		lock_refresh = true;
-		$('.mdl-grid').append('<div class="mdl-spinner mdl-js-spinner is-active"></div>');
+		$('.mdl-grid').append('<div class="mdl-spinner mdl-js-spinner is-active"></div>');componentHandler.upgradeDom('MaterialSpinner', 'mdl-spinner');
 		var params = [];
 		params.push({'name': csrf_token_name, 'value': $.cookie(csrf_cookie_name)});
 		pagination = pagination + 10;
