01c5ad240550c0f2bc44a3e3af46400c15e7383a
project29k <divers@sdion.net>
2015-08-08 09:53:35 +0200

    -

--- scripts/member.js
+++ scripts/member.js
@@ -166,11 +166,12 @@ function item_expand(ref) {
 		statusCode: {
 			200: function(data_return, textStatus, jqXHR) {
 				if(data_return.itm_content) {
-					$('#item_' + data_return.itm_id).find('.expand').parent().hide();
-					$('#item_' + data_return.itm_id).find('.collapse').parent().show();
-					$('#item_' + data_return.itm_id).find('.item-content').html(data_return.itm_content);
-					$('#item_' + data_return.itm_id).find('.item-content').show();
+					$('#item_' + data_return.itm_id).find('.expand').hide();
+					$('#item_' + data_return.itm_id).find('.collapse').show();
+					$('#item_' + data_return.itm_id).find('.mdl-card__supporting-text').html(data_return.itm_content);
+					$('#item_' + data_return.itm_id).find('.mdl-card__supporting-text').show();
 					$('#item_' + data_return.itm_id).removeClass('collapse');
+					$('#item_' + data_return.itm_id).addClass('expand');
 					scroll_to('#item_' + data_return.itm_id);
 				}
 			}
@@ -180,28 +181,23 @@ function item_expand(ref) {
 	});
 }
 function item_collapse(ref) {
-	ref.find('.item-content').hide();
-	ref.find('.item-content').html('');
-	ref.find('.collapse').parent().hide();
-	ref.find('.expand').parent().show();
+	ref.find('.mdl-card__supporting-text').hide();
+	ref.find('.mdl-card__supporting-text').html('');
+	ref.find('.collapse').hide();
+	ref.find('.expand').show();
+	ref.removeClass('expand');
 	ref.addClass('collapse');
 	scroll_to('#' + ref.attr('id'));
 }
 function items_collapse() {
-	var ref = $('.items_display');
-	ref.find('.collapse').hide();
-	ref.find('.expand').show();
 	items_display = 'collapse';
 	$.cookie('items_display', items_display, { expires: 30, path: '/' });
-	load_items(url);
+	load_items( $('.mdl-navigation').find('li.active').find('a.mdl-navigation__link').attr('href') );
 }
 function items_expand() {
-	var ref = $('.items_display');
-	ref.find('.expand').hide();
-	ref.find('.collapse').show();
 	items_display = 'expand';
 	$.cookie('items_display', items_display, { expires: 30, path: '/' });
-	load_items(url);
+	load_items( $('.mdl-navigation').find('li.active').find('a.mdl-navigation__link').attr('href') );
 }
 $(document).ready(function() {
 	set_positions();
