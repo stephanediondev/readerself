a0682eef557ed945f39c4e810d42ef1d2633d5a5
project29k <divers@sdion.net>
2015-08-09 20:33:50 +0200

    -

--- application/views/settings_index.php
+++ application/views/settings_index.php
@@ -35,6 +35,30 @@
 	</div>
 
 	<?php if($this->member->mbr_administrator == 1) { ?>
+		<div class="mdl-grid">
+			<div class="mdl-card mdl-color--red mdl-cell mdl-cell--2-col" title="Red"><div class="mdl-card__supporting-text mdl-color-text--white">red<br>#F44336</div></div>
+			<div class="mdl-card mdl-color--pink mdl-cell mdl-cell--2-col" title="Pink"><div class="mdl-card__supporting-text mdl-color-text--white">pink<br>#E91E63</div></div>
+			<div class="mdl-card mdl-color--purple mdl-cell mdl-cell--2-col" title="Purple"><div class="mdl-card__supporting-text mdl-color-text--white">purple<br>#9C27B0</div></div>
+			<div class="mdl-card mdl-color--deep-purple mdl-cell mdl-cell--2-col" title="Deep Purple"><div class="mdl-card__supporting-text mdl-color-text--white">deep-purple<br>#673AB7</div></div>
+			<div class="mdl-card mdl-color--indigo mdl-cell mdl-cell--2-col" title="Indigo"><div class="mdl-card__supporting-text mdl-color-text--white">indigo<br>#3F51B5</div></div>
+			<div class="mdl-card mdl-color--blue mdl-cell mdl-cell--2-col" title="Blue"><div class="mdl-card__supporting-text mdl-color-text--white">blue<br>#2196F3</div></div>
+			<div class="mdl-card mdl-color--light-blue mdl-cell mdl-cell--2-col" title="Light Blue"><div class="mdl-card__supporting-text mdl-color-text--black">light-blue<br>#03A9F4</div></div>
+			<div class="mdl-card mdl-color--cyan mdl-cell mdl-cell--2-col" title="Cyan"><div class="mdl-card__supporting-text mdl-color-text--black">cyan<br>#00BCD4</div></div>
+			<div class="mdl-card mdl-color--teal mdl-cell mdl-cell--2-col" title="Teal"><div class="mdl-card__supporting-text mdl-color-text--white">teal<br>#009688</div></div>
+			<div class="mdl-card mdl-color--green mdl-cell mdl-cell--2-col" title="Green"><div class="mdl-card__supporting-text mdl-color-text--black">green<br>#4CAF50</div></div>
+			<div class="mdl-card mdl-color--light-green mdl-cell mdl-cell--2-col" title="Light Green"><div class="mdl-card__supporting-text mdl-color-text--black">light-green<br>#8BC34A</div></div>
+			<div class="mdl-card mdl-color--lime mdl-cell mdl-cell--2-col" title="Lime"><div class="mdl-card__supporting-text mdl-color-text--black">lime<br>#CDDC39</div></div>
+			<div class="mdl-card mdl-color--yellow mdl-cell mdl-cell--2-col" title="Yellow"><div class="mdl-card__supporting-text mdl-color-text--black">yellow<br>#FFEB3B</div></div>
+			<div class="mdl-card mdl-color--amber mdl-cell mdl-cell--2-col" title="Amber"><div class="mdl-card__supporting-text mdl-color-text--black">amber<br>#FFC107</div></div>
+			<div class="mdl-card mdl-color--orange mdl-cell mdl-cell--2-col" title="Orange"><div class="mdl-card__supporting-text mdl-color-text--black">orange<br>#FF9800</div></div>
+			<div class="mdl-card mdl-color--deep-orange mdl-cell mdl-cell--2-col" title="Deep Orange"><div class="mdl-card__supporting-text mdl-color-text--white">deep-orange<br>#FF5722</div></div>
+			<div class="mdl-card mdl-color--brown mdl-cell mdl-cell--2-col" title="Brown"><div class="mdl-card__supporting-text mdl-color-text--white">brown<br>#795548</div></div>
+			<div class="mdl-card mdl-color--grey mdl-cell mdl-cell--2-col" title="Grey"><div class="mdl-card__supporting-text mdl-color-text--black">grey<br>#9E9E9E</div></div>
+			<div class="mdl-card mdl-color--blue-grey mdl-cell mdl-cell--2-col" title="Blue Grey"><div class="mdl-card__supporting-text mdl-color-text--white">blue-grey<br>#607D8B</div></div>
+			<div class="mdl-card mdl-color--black mdl-cell mdl-cell--2-col" title="Black"><div class="mdl-card__supporting-text mdl-color-text--white">black<br>#000000</div></div>
+			<div class="mdl-card mdl-color--white mdl-cell mdl-cell--2-col" title="White"><div class="mdl-card__supporting-text mdl-color-text--black">white<br>#FFFFFF</div></div>
+		</div>
+
 		<?php echo form_open(current_url()); ?>
 		<div class="mdl-grid">
 			<div class="mdl-card mdl-color--<?php echo $this->config->item('material-design/colors/background/card'); ?> mdl-cell mdl-cell--6-col">
