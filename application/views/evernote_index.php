45767be7df301a6b82373f843115b30004f96e80
project29k <divers@sdion.net>
2015-08-13 13:54:42 +0200

    -

--- application/views/evernote_index.php
+++ application/views/evernote_index.php
@@ -74,5 +74,24 @@
 				</div>
 			</div>
 		<?php } ?>
+
+		<?php if($this->member->mbr_administrator == 1) { ?>
+			<div class="mdl-card mdl-shadow--2dp mdl-color--<?php echo $this->config->item('material-design/colors/background/card'); ?> mdl-cell mdl-cell--12-col">
+				<div class="mdl-card__title mdl-color-text--<?php echo $this->config->item('material-design/colors/text/card-title'); ?>">
+					<h1 class="mdl-card__title-text"><?php if($token) { ?><i class="material-icons md-18">done</i><?php } ?>Tutorial</h1>
+				</div>
+				<div class="mdl-card__supporting-text mdl-color-text--<?php echo $this->config->item('material-design/colors/text/content'); ?>">
+					<p>Go to <a href="https://dev.evernote.com/" target="blank">https://dev.evernote.com/</a> and get an API Key.</p>
+					<p><img src="medias/evernote_get_api_key.png"></p>
+	
+					<p><a href="<?php echo base_url(); ?>settings/other">Edit settings</a> and fill in "Evernote / Consumer Key" and "Evernote / Consumer Secret" with the values that you received from Evernote.</p>
+					<p><img src="medias/evernote_settings.png"></p>
+	
+					<p>Go to <a href="https://dev.evernote.com/support/" target="blank">https://dev.evernote.com/support/</a> and activate your API Key.</p>
+					<p><img src="medias/evernote_activate_api_key.png"></p>
+				</div>
+			</div>
+		<?php } ?>
+
 	</div>
 </main>
