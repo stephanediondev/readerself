45628b1c5d465baef069a4359bc1ed4394b3ce24
project29k <divers@sdion.net>
2015-08-26 23:33:01 +0200

    -

--- application/views/elasticsearch_form.php
+++ application/views/elasticsearch_form.php
@@ -1,6 +1,6 @@
 <main class="mdl-layout__content mdl-color--<?php echo $this->config->item('material-design/colors/background/layout'); ?>">
 	<div class="mdl-grid">
-		<div class="mdl-card mdl-shadow--2dp mdl-color--<?php echo $this->config->item('material-design/colors/background/card'); ?> mdl-cell mdl-cell--12-col">
+		<div class="mdl-card mdl-shadow--2dp mdl-color--<?php echo $this->config->item('material-design/colors/background/card'); ?> mdl-cell mdl-cell--4-col">
 			<div class="mdl-card__title mdl-color-text--<?php echo $this->config->item('material-design/colors/text/card-title-highlight'); ?> mdl-color--<?php echo $this->config->item('material-design/colors/background/card-title-highlight'); ?>">
 				<?php if($this->input->get('q')) { ?>
 					<h1 class="mdl-card__title-text"><i class="material-icons md-18">search</i>Elasticsearch (<?php echo $hits->total; ?>)</h1>
